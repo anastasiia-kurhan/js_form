3b9b607a6d39fe5469e466972293a4fa62aba057
Anastasiia Kurhan <65221638+Anastasiia-Kurhan@users.noreply.github.com>
2024-01-28 16:50:41 +0200

    Replace the attributes with an event listener

--- js/index.js
+++ js/index.js
@@ -1,9 +1,14 @@
-function checkForm(el) {
+document.getElementById('myform').addEventListener('submit', function (e) {
+    e.preventDefault(); //видаляє стандартну поведінку сторінки (не буде перезавантажуватись)
+
+    var el = document.getElementById('myform'); //знаходимо форму
+    //знаходимо усі поля
     var username = el.username.value;
     var email = el.email.value;
     var pass = el.password.value;
     var gender = el.gender.value;
     
+    //перевіряємо усі поля
     var error = '';
     if (username.length < 2)
         error = 'Ім\'я занадто мале';
@@ -16,13 +21,15 @@ function checkForm(el) {
     else if (gender == null || gender == '')
         error = 'Невизначений гендер';
 
+    //якщо є помилка то видображуємо її
     if (error != '') {
         document.getElementById('error').innerText = error;
         return false;
     }
 
-    document.getElementById('error').innerText = '';
-    window.location = 'https://www.google.com.ua/?hl=uk'
+    document.getElementById('error').innerText = ''; //прибираємо текст помилки
+    window.location = 'https://www.google.com.ua/?hl=uk' //виконуємо переадресацію якщо все добре
 
     return false;
-}
+})
+
