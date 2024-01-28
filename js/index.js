964805d28b2cc4748439fa833df58b33db0ed503
Anastasiia Kurhan <65221638+Anastasiia-Kurhan@users.noreply.github.com>
2024-01-28 16:15:53 +0200

    Added validation to each form item

--- js/index.js
+++ js/index.js
@@ -1,6 +1,25 @@
-function checkForm() {
-    var username = document.getElementById('username').value;
-    console.log(username);
+function checkForm(el) {
+    var username = el.username.value;
+    var email = el.email.value;
+    var pass = el.password.value;
+    var gender = el.gender.value;
+    
+    var error = '';
+    if (username.length < 2)
+        error = 'Ім\'я занадто мале';
+    else if (email.length < 2)
+        error = 'Email занадто малий';
+    else if (!email.includes('@'))
+        error = 'Email не має @';
+    else if (pass.length < 2)
+        error = 'Пароль занадто малий';
+    else if (gender == null || gender == '')
+        error = 'Невизначений гендер';
 
+    if (error != '') {
+        document.getElementById('error').innerText = error;
+        return false;
+    }
+        
     return false;
 }
